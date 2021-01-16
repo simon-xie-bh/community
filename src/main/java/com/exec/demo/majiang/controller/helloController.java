0e75554a9eb17eee4897286d90eaa5f0963cace2
simon <bh@163.com>
2021-01-16 11:59:26 +0800

    first

---
+++ src/main/java/com/exec/demo/majiang/controller/helloController.java
@@ -0,0 +1,18 @@
+package com.exec.demo.majiang.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class helloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
